3574f5a9de9af5306c5c00ec56146af8c77fc62c
Kostya Bondarenko <61065186+Spook-tech@users.noreply.github.com>
2023-04-16 11:10:00 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,11 +4,14 @@ const headerMenu = document.querySelector(".header-mobile");
 function toggleHeaderMenu(){
   menuBurger.classList.toggle("active");
   headerMenu.classList.toggle("active");
-  document.body.classList.toggle("lock");
 }
 
 document.addEventListener("click", function(event){
-  if(event.target.closest(".header-burger")){
+  const target = event.target;
+
+  console.log(target);
+
+  if(target.closest(".header-burger-wrapper")){
     toggleHeaderMenu();
   }
 });
